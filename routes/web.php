a2c5c3b32113420324d8660786fd7baca8ef0017
3A532053 <showhk61@gmail.com>
2018-11-14 14:43:10 +0800

    練習 6：了解 Model 和 Collection 的差異

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/', function () {
 //    $posts = \App\Post::all();
  //   dd($posts);
 //    \App\Post::destroy(2);
-      \App\Post::destroy(3, 5, 7);
+//      \App\Post::destroy(3, 5, 7);
 //    $post->title = 'saved title';
 //    $post->content = 'saved content';
 
@@ -35,6 +35,18 @@ Route::get('/', function () {
 //    return view('welcome');
 //    $posts = \App\Post::where('id', '<', 10)->orderBy('id', 'DESC')->get();
 //    dd($posts);
+
+//    $allPosts = \App\Post::all();
+//    dd($allPosts);
+
+//    $featuredPosts = \App\Post::where('is_feature', 1)->get();
+//    dd($featuredPosts);
+
+    $fourthPost = \App\Post::find(4);
+    dd($fourthPost);
+
+    $lastPost = \App\Post::orderBy('id', 'DESC')->first();
+    dd($lastPost);
 });
 Route::get('/home', 'HomeController@index');
 Route::get('/hello/{name?}', ['as'=>'hello.index', 'uses'=>'HelloController@index']);
